a90da5701a885a93b2679e01d0a14f0c4a2e4dd0
alanngeorge1 <alanngeorge1@gmail.com>
2020-05-02 02:21:36 -0300

    Requisição GET e POST

---
+++ src/main/java/github/com/alanngeorge1/rest/controller/ClienteController.java
@@ -0,0 +1,36 @@
+package github.com.alanngeorge1.rest.controller;
+
+import github.com.alanngeorge1.domain.entity.Cliente;
+import github.com.alanngeorge1.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+public class ClienteController {
+
+private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity <Cliente> getClienteByid(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+        if (cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @PostMapping("api/clientes")
+    @ResponseBody
+    public ResponseEntity save (@RequestBody Cliente cliente ){
+        Cliente clienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
+
+    }
+
+}
