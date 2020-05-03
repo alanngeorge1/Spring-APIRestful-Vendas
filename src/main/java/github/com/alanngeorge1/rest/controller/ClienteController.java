c93c24ca60b6fc8fce30d1c9a806a034308decce
alanngeorge1 <alanngeorge1@gmail.com>
2020-05-03 20:19:59 -0300

    Metodo Delete

--- src/main/java/github/com/alanngeorge1/rest/controller/ClienteController.java
+++ src/main/java/github/com/alanngeorge1/rest/controller/ClienteController.java
@@ -11,26 +11,39 @@ import java.util.Optional;
 @Controller
 public class ClienteController {
 
-private Clientes clientes;
+    private Clientes clientes;
 
     public ClienteController(Clientes clientes) {
         this.clientes = clientes;
     }
+
     @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public ResponseEntity <Cliente> getClienteByid(@PathVariable Integer id){
+    public ResponseEntity<Cliente> getClienteByid(@PathVariable Integer id) {
         Optional<Cliente> cliente = clientes.findById(id);
-        if (cliente.isPresent()){
+        if (cliente.isPresent()) {
             return ResponseEntity.ok(cliente.get());
         }
         return ResponseEntity.notFound().build();
     }
+
     @PostMapping("api/clientes")
     @ResponseBody
-    public ResponseEntity save (@RequestBody Cliente cliente ){
+    public ResponseEntity save(@RequestBody Cliente cliente) {
         Cliente clienteSalvo = clientes.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
 
     }
 
+    @DeleteMapping("api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+        if (cliente.isPresent()) {
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+
+    }
 }
