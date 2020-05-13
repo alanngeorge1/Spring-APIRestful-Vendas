547f1b1019e04ffe5a277360fdef3765d43d73fd
alanngeorge1 <alanngeorge1@gmail.com>
2020-05-12 21:19:16 -0300

    HandleException

---
+++ src/main/java/github/com/alanngeorge1/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,17 @@
+package github.com.alanngeorge1.rest.controller;
+
+import github.com.alanngeorge1.exception.RegraNegocioException;
+import github.com.alanngeorge1.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex){
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
